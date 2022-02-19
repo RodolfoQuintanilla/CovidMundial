712e97f029fd3250f61db8d86058db22635ce939
Rodolfo <rqm@outlook.es>
2022-02-19 15:12:39 -0600

    Rutas

--- sistema/src/App.js
+++ sistema/src/App.js
@@ -1,9 +1,21 @@
+import Layaout from "./layaout.js/Layaout";
+import { Routes, Route } from "react-router-dom";
+import VistaGeneral from "./components/VistaGeneral";
+import Nav from './components/Nav'
+import Test from "./components/Test";
+
 
 
 function App() {
   return (
     <div >
-      Hola Perro
+      <Nav />
+      <Routes>
+        <Route index element={<VistaGeneral />} />
+        <Route path="Test" element={<Test />} />
+      </Routes>
+      <Layaout />
+
     </div>
   );
 }
---
+++ sistema/src/components/VistaGeneral.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VistaGeneral = () => {
+    return (
+        <div>
+            <h1>Desde Vista General</h1>
+        </div>
+    );
+};
+
+export default VistaGeneral;
